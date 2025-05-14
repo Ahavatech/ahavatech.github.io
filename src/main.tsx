bb70b011a3b3792cdb8da23bc988cf990029c6d7
[Ibukunoluwa Adekunle] <[ibkadekunle2004@gmail.com]>
2025-05-14 15:02:21 +0100

    fix deploy yml

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@lib/queryClient'
 import App from './App'
-
-const queryClient = new QueryClient()
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
